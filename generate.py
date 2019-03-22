b36d3b53ceda9212b604448d80258d05cf4e8811
Fanny <fanny.salvail-berard@usherbrooke.ca>
2019-03-22 11:55:16 -0400

    #5

---
+++ generate.py
@@ -0,0 +1,168 @@
+#!/bin/python
+# coding: utf-8
+
+import argparse
+import time
+import collections
+import os
+import sys
+import torch
+import torch.nn
+from torch.autograd import Variable
+import torch.nn as nn
+import numpy
+
+np = numpy
+
+# NOTE ==============================================
+# This is where your models are imported
+from models import RNN, GRU
+from models import make_model as TRANSFORMER
+
+
+parser = argparse.ArgumentParser(description='PyTorch Penn Treebank Language Modeling')
+
+# Arguments you may need to set to run different experiments in 4.1 & 4.2.
+parser.add_argument('--data', type=str, default='data',
+                    help='location of the data corpus. We suggest you change the default\
+                    here, rather than passing as an argument, to avoid long file paths.')
+parser.add_argument('--model', type=str, default='RNN',
+                    help='type of recurrent net (RNN, GRU, TRANSFORMER)')
+parser.add_argument('--optimizer', type=str, default='SGD_LR_SCHEDULE',
+                    help='optimization algo to use; SGD, SGD_LR_SCHEDULE, ADAM')
+parser.add_argument('--seq_len', type=int, default=35,
+                    help='number of timesteps over which BPTT is performed')
+parser.add_argument('--batch_size', type=int, default=20,
+                    help='size of one minibatch')
+parser.add_argument('--initial_lr', type=float, default=20.0,
+                    help='initial learning rate')
+parser.add_argument('--hidden_size', type=int, default=200,
+                    help='size of hidden layers. IMPORTANT: for the transformer\
+                    this must be a multiple of 16.')
+parser.add_argument('--save_best', action='store_true',
+                    help='save the model for the best validation performance')
+parser.add_argument('--num_layers', type=int, default=2,
+                    help='number of hidden layers in RNN/GRU, or number of transformer blocks in TRANSFORMER')
+
+# Other hyperparameters you may want to tune in your exploration
+parser.add_argument('--emb_size', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--num_epochs', type=int, default=40,
+                    help='number of epochs to stop after')
+parser.add_argument('--dp_keep_prob', type=float, default=0.35,
+                    help='dropout *keep* probability. drop_prob = 1-dp_keep_prob \
+                    (dp_keep_prob=1 means no dropout)')
+
+# Arguments that you may want to make use of / implement more code for
+parser.add_argument('--debug', action='store_true')
+parser.add_argument('--save_dir', type=str, default='',
+                    help='path to save the experimental config, logs, model \
+                    This is automatically generated based on the command line \
+                    arguments you pass and only needs to be set if you want a \
+                    custom dir name')
+parser.add_argument('--evaluate', action='store_true',
+                    help="use this flag to run on the test set. Only do this \
+                    ONCE for each model setting, and only after you've \
+                    completed ALL hyperparameter tuning on the validation set.\
+                    Note we are not requiring you to do this.")
+
+# DO NOT CHANGE THIS (setting the random seed makes experiments deterministic, 
+# which helps for reproducibility)
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+
+args = parser.parse_args()
+argsdict = args.__dict__
+argsdict['code_file'] = sys.argv[0]
+
+# Use the GPU if you have one
+if torch.cuda.is_available():
+    print("Using the GPU")
+    device = torch.device("cuda")
+else:
+    print("WARNING: You are about to run on cpu, and this will likely run out \
+      of memory. \n You can try setting batch_size=1 to reduce memory usage")
+    device = torch.device("cpu")
+
+
+# HELPER FUNCTIONS
+def _read_words(filename):
+    with open(filename, "r") as f:
+        return f.read().replace("\n", "<eos>").split()
+
+
+def _build_vocab(filename):
+    data = _read_words(filename)
+
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+    id_to_word = dict((v, k) for k, v in word_to_id.items())
+
+    return word_to_id, id_to_word
+
+
+def _file_to_word_ids(filename, word_to_id):
+    data = _read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+# Processes the raw data from text files
+def ptb_raw_data(data_path=None, prefix="ptb"):
+    train_path = os.path.join(data_path, prefix + ".train.txt")
+    valid_path = os.path.join(data_path, prefix + ".valid.txt")
+    test_path = os.path.join(data_path, prefix + ".test.txt")
+
+    word_to_id, id_2_word = _build_vocab(train_path)
+    train_data = _file_to_word_ids(train_path, word_to_id)
+    valid_data = _file_to_word_ids(valid_path, word_to_id)
+    test_data = _file_to_word_ids(test_path, word_to_id)
+    return train_data, valid_data, test_data, word_to_id, id_2_word
+
+
+# LOAD DATA
+print('Loading data from ' + args.data)
+raw_data = ptb_raw_data(data_path=args.data)
+train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
+vocab_size = len(word_to_id)
+print('  vocabulary size: {}'.format(vocab_size))
+
+# NOTE ==============================================
+# This is where your model code will be called. You may modify this code
+# if required for your implementation, but it should not typically be necessary,
+# and you must let the TAs know if you do so.
+if args.model == 'RNN':
+    model = RNN(emb_size=args.emb_size, hidden_size=args.hidden_size,
+                seq_len=args.seq_len, batch_size=args.batch_size,
+                vocab_size=vocab_size, num_layers=args.num_layers,
+                dp_keep_prob=args.dp_keep_prob)
+elif args.model == 'GRU':
+    model = GRU(emb_size=args.emb_size, hidden_size=args.hidden_size,
+                seq_len=args.seq_len, batch_size=args.batch_size,
+                vocab_size=vocab_size, num_layers=args.num_layers,
+                dp_keep_prob=args.dp_keep_prob)
+elif args.model == 'TRANSFORMER':
+    if args.debug:  # use a very small model
+        model = TRANSFORMER(vocab_size=vocab_size, n_units=16, n_blocks=2)
+    else:
+        # Note that we're using num_layers and hidden_size to mean slightly 
+        # different things here than in the RNNs.
+        # Also, the Transformer also has other hyperparameters 
+        # (such as the number of attention heads) which can change it's behavior.
+        model = TRANSFORMER(vocab_size=vocab_size, n_units=args.hidden_size,
+                            n_blocks=args.num_layers, dropout=1. - args.dp_keep_prob)
+        # these 3 attributes don't affect the Transformer's computations;
+    # they are only used in run_epoch
+    model.batch_size = args.batch_size
+    model.seq_len = args.seq_len
+    model.vocab_size = vocab_size
+else:
+    print("Model type not recognized.")
+
+model = model.to(device)
+print("device is = ", model)
+
+print("Load model parameters, best_params.pt")
+model.load_state_dict(torch.load(os.path.join(args.save_dir, 'best_params.pt')))
+model.eval()
